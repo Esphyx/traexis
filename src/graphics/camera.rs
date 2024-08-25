6d68f4ed590586cc0c6f646c4ca3b62efd354d5d
Esphyx <esphyxis@gmail.com>
2024-08-25 14:16:01 +0200

    Added a cross product function and a matrices calculation to Camera

--- src/graphics/camera.rs
+++ src/graphics/camera.rs
@@ -2,6 +2,28 @@ pub struct Camera {
     position: [f32; 3],
     target: [f32; 3],
     up: [f32; 3],
+    fov: f32,
+}
+
+impl Camera {
+    pub fn perspective(&self, width: u32, height: u32) -> [[f32; 4]; 4] {
+        let aspect_ratio = height as f32 / width as f32;
+        let focal_length = 1.0 / (self.fov / 2.0).tan();
+
+        const ZNEAR: f32 = 0.1;
+        const ZFAR: f32 = 1024.0;
+
+        [
+            [focal_length * aspect_ratio, 0.0, 0.0, 0.0],
+            [0.0, focal_length, 0.0, 0.0],
+            [0.0, 0.0, (ZFAR + ZNEAR) / (ZFAR - ZNEAR), 1.0],
+            [0.0, 0.0, -(2.0 * ZFAR * ZNEAR) / (ZFAR - ZNEAR), 0.0],
+        ]
+    }
+
+    pub fn view_matrix(&self) -> [[f32; 4]; 4] {
+        let z_axis = -
+    }
 }
 
 impl Default for Camera {
--- src/graphics/linear_algebra.rs
+++ src/graphics/linear_algebra.rs
@@ -17,3 +17,11 @@ pub fn subtract(first: [f32; 3], second: [f32; 3]) -> [f32; 3] {
 pub fn negate(vector: [f32; 3]) -> [f32; 3] {
     [-vector[0], -vector[1], -vector[2]]
 }
+
+pub fn cross(first: [f32; 3], second: [f32; 3]) -> [f32; 3] {
+    [
+        first[1] * second[2] - first[2] * second[1],
+        first[2] * second[0] - first[0] * second[2],
+        first[0] * second[1] - first[1] * second[0],
+    ]
+}
